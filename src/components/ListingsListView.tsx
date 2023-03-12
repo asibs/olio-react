9b53507b08bd5e226c7b1ada69f1b7676593cdc7
asibs <andrew@organise.network>
2023-03-12 20:40:16 +0000

    Removing more unused imports

--- src/components/ListingsListView.tsx
+++ src/components/ListingsListView.tsx
@@ -4,8 +4,6 @@ import ListingCard from './ListingCard';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
 
 interface Props {
   listings: Listing[];
